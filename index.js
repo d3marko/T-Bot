4a9b79b366151a7380b3f04f812e293da716b211
Mayo <50989212+d3marko@users.noreply.github.com>
2021-06-08 00:08:51 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,24 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone: true});
 const config = require("./config.json");
 
-
-
 client.on("ready", () => {
   console.log(`Bot state: started\n 
 	| STATS:\n| LOG: bot started with: ${client.users.cache.size} users.\n| LOG: bot started in: ${client.channels.cache.size} channels.\n| LOG: bot started on: ${client.guilds.cache.size} guilds.`);
 
-client.user.setPresence({ activity: { name: 'beta' }, status: 'online' })
-});
+client.user.setPresence({ activity: { name: `${client.users.cache.size}` }, status: 'online' })
 
-client.on("message", async message => {
  
-  if(!message.content.startsWith(config.prefix)) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-
 });
-
 
 client.login(config.token);
